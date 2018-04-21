2f4a8ee98249185a594363951df0aef31cc8f67e
ValentineGladyshko <valentine9022@gmail.com>
2018-04-21 14:31:58 +0300

    Added lab3

---
+++ KPI-Semester-6/lab3/CollectionLib/CollectionLib.cs
@@ -0,0 +1,241 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+
+namespace Collection.Lib
+{
+    public class Node<T>
+    {
+        public Node(T data)
+        {
+            Data = data;
+        }
+        public T Data { get; set; }
+        public Node<T> Next { get; set; }
+
+        public override string ToString()
+        {
+            return Data.ToString();
+        }
+    }
+
+    public class MyQueue<T> : IEnumerable<T>, ICollection
+    {
+        Node<T> head;
+        Node<T> tail;
+        int count;
+
+        public class TEventArgs : EventArgs
+        {
+            public T data;
+
+            public TEventArgs(T data)
+            {
+                this.data = data;
+            }
+        };
+
+        public delegate void ClearHandler(object obj, EventArgs args);
+        public delegate void DequeueHandler(object obj, TEventArgs args);
+        public delegate void EnqueueHandler(object obj, TEventArgs args);
+
+        public event ClearHandler DoClear;
+        public event DequeueHandler DoDequeue;
+        public event EnqueueHandler DoEnqueue;
+
+        public void OnDoClear()
+        {
+            EventArgs args = new EventArgs();
+            DoClear?.Invoke(this, args);
+        }
+
+        public void OnDoDequeue(T data)
+        {
+            TEventArgs args = new TEventArgs(data);
+            DoDequeue?.Invoke(this, args);
+        }
+
+        public void OnDoEnqueue(T data)
+        {
+            TEventArgs args = new TEventArgs(data);
+            DoEnqueue?.Invoke(this, args);
+        }
+
+        public MyQueue()
+        {
+            head = null;
+            tail = null;
+            count = 0;
+        }
+
+        public T Dequeue()
+        {
+            if (count == 0)
+            {
+                throw new InvalidOperationException("Empty collection.");
+            }
+
+            T output = head.Data;
+            OnDoDequeue(output);
+            head = head.Next;
+            count--;
+
+            return output;
+        }
+
+        public void Enqueue(T data)
+        {
+            Node<T> node = new Node<T>(data);
+            Node<T> temp = tail;
+            tail = node;
+            OnDoEnqueue(data);
+            if (count == 0)
+            {
+                head = tail;
+            }
+            else
+            {
+                temp.Next = tail;
+            }
+
+            count++;
+        }
+
+        public T Peek()
+        {
+            if (count == 0)
+            {
+                throw new InvalidOperationException("Empty collection.");
+            }
+
+            T output = head.Data;
+            return output;
+        }
+
+        public T[] ToArray()
+        {
+            T[] output = new T[count];
+
+            Node<T> temp = head;
+            int i = 0;
+            while (temp != null)
+            {
+                output[i++] = temp.Data;
+                temp = temp.Next;
+            }
+            return output;
+        }
+
+        public void TrimExcess() { }
+
+        public bool Contains(T data)
+        {
+            Node<T> temp = head;
+            while (temp != null)
+            {
+                if (temp.Data.Equals(data))
+                    return true;
+                temp = temp.Next;
+            }
+            return false;
+        }
+
+        public void Clear()
+        {
+            head = null;
+            tail = null;
+            count = 0;
+            OnDoClear();
+        }
+
+        public void CopyTo(T[] array, int arrayIndex)
+        {
+            if (array == null)
+            {
+                throw new ArgumentNullException();
+            }
+
+            if (arrayIndex < 0)
+            {
+                throw new ArgumentOutOfRangeException();
+            }
+
+            if ((array.Length - arrayIndex) < count)
+            {
+                throw new ArgumentException("Array is small.");
+            }
+
+            Node<T> temp = head;
+            int i = arrayIndex;
+            while (temp != null)
+            {
+                array[i++] = temp.Data;
+                temp = temp.Next;
+            }
+        }
+
+        public int Count { get { return count; } }
+
+        public object SyncRoot => throw new NotImplementedException("I don't know async programming.");
+
+        public bool IsSynchronized => throw new NotImplementedException("I don't know async programming.");
+
+        public void CopyTo(Array array, int index)
+        {
+            if (array == null)
+            {
+                throw new ArgumentNullException();
+            }
+
+            if (index < 0)
+            {
+                throw new ArgumentOutOfRangeException();
+            }
+
+            if ((array.Length - index) < count)
+            {
+                throw new ArgumentException("Array is small.");
+            }
+
+            Node<T> temp = head;
+            int i = index;
+            while (temp != null)
+            {
+                array.SetValue(temp.Data, i++);
+                temp = temp.Next;
+            }
+        }
+
+        IEnumerator IEnumerable.GetEnumerator()
+        {
+            return GetEnumerator();
+        }
+
+        public IEnumerator<T> GetEnumerator()
+        {
+            Node<T> temp = head;
+            while (temp != null)
+            {
+                yield return temp.Data;
+                temp = temp.Next;
+            }
+        }
+
+        public override string ToString()
+        {
+            string output = "";
+            output += "\nQueue:\n\tCount: " + count.ToString() + "\n\tData: ";
+
+            Node<T> temp = head;
+            while (temp != null)
+            {
+                if (temp.Next != null)
+                    output += temp.Data.ToString() + " ";
+                else
+                    output += temp.Data.ToString();
+                temp = temp.Next;
+            }
+            return output.ToString();
+        }
+    }
+}
---
+++ KPI-Semester-6/lab3/CollectionLibTests/MyQueueTests.cs
@@ -0,0 +1,325 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using System;
+
+namespace Collection.Lib.Tests
+{
+    [TestClass]
+    public class MyQueueTests
+    {
+        [TestMethod]
+        [ExpectedException(typeof(InvalidOperationException))]
+
+        public void Dequeue_DequeueFromEmptyQueue_ShouldThrowInvalidOperationException()
+        {
+            var queue = new MyQueue<int>();
+
+            queue.Dequeue();
+        }
+
+        [TestMethod]
+
+        public void Dequeue_Dequeue3FromQueue_3Returned()
+        {
+            var queue = new MyQueue<int>();
+            queue.Enqueue(3);
+            queue.Enqueue(6);
+
+            var num = queue.Dequeue();
+
+            NUnit.Framework.Assert.AreEqual(3, num);
+        }
+
+        [TestMethod]
+
+        public void Dequeue_Dequeue3FromQueue_QueueIsEmpty()
+        {
+            var queue = new MyQueue<int>();
+            queue.Enqueue(3);
+
+            queue.Dequeue();
+
+            NUnit.Framework.CollectionAssert.IsEmpty(queue);
+        }
+
+        [TestMethod]
+
+        public void Enqueue_Enqueue3ToQueue_3InTheHeadOfQueue()
+        {
+            var queue = new MyQueue<int>();
+
+            queue.Enqueue(3);
+            queue.Enqueue(4);
+
+            NUnit.Framework.Assert.AreEqual(queue.Peek(), 3);
+            //NUnit.Framework.Assert.Contains(3, queue);
+        }
+
+        [TestMethod]
+        [ExpectedException(typeof(InvalidOperationException))]
+
+        public void Peek_PeekFromEmptyQueue_ShouldThrowInvalidOperationException()
+        {
+            var queue = new MyQueue<int>();
+
+            queue.Peek();
+        }
+
+        [TestMethod]
+
+        public void Peek_PeekFromQueue_3Returned()
+        {
+            var queue = new MyQueue<int>();
+            queue.Enqueue(3);
+            queue.Enqueue(4);
+
+            var num = queue.Peek();
+
+            NUnit.Framework.Assert.AreEqual(3, num);
+        }
+
+        [TestMethod]
+
+        public void ToArray_IntQueueToArray_IntArrayReturned()
+        {
+            var queue = new MyQueue<int>();
+            queue.Enqueue(3);
+            queue.Enqueue(-6);
+
+            var array = queue.ToArray();
+
+            NUnit.Framework.Assert.IsInstanceOf(typeof(int[]), array);
+        }
+
+        [TestMethod]
+
+        public void ToArray_QueueWithValuesToArray_ArrayWithValuesReturned()
+        {
+            var queue = new MyQueue<int>();
+            queue.Enqueue(3);
+            queue.Enqueue(-6);
+
+            var array = queue.ToArray();
+
+            NUnit.Framework.CollectionAssert.AreEqual(array, queue);
+        }
+
+        [TestMethod]
+
+        public void Contains_QueueContains6_TrueReturned()
+        {
+            var queue = new MyQueue<int>();
+            queue.Enqueue(-3);
+            queue.Enqueue(6);
+
+            var flag = queue.Contains(6);
+
+            NUnit.Framework.Assert.IsTrue(flag);
+        }
+
+        [TestMethod]
+
+        public void Contains_QueueContains6_FalseReturned()
+        {
+            var queue = new MyQueue<int>();
+            queue.Enqueue(-3);
+            queue.Enqueue(-6);
+
+            var flag = queue.Contains(6);
+
+            NUnit.Framework.Assert.IsFalse(flag);
+        }
+
+        [TestMethod]
+
+        public void Clear_ClearQueue_QueueIsEmpty()
+        {
+            var queue = new MyQueue<int>();
+            queue.Enqueue(3);
+            queue.Enqueue(-2);
+
+            queue.Clear();
+
+            NUnit.Framework.CollectionAssert.IsEmpty(queue);
+        }
+
+        [TestMethod]
+
+        public void CopyTo_CopyToNullArray_ShouldThrowArgumentNullException()
+        {
+            var queue = new MyQueue<int>();
+            queue.Enqueue(3);
+            queue.Enqueue(-2);
+            int[] array = null;
+
+            try
+            {
+                queue.CopyTo(array, 0);
+            }
+
+            catch(ArgumentNullException)
+            {
+                return;
+            }
+
+            NUnit.Framework.Assert.Fail();
+        }
+
+        [TestMethod]
+        [ExpectedException(typeof(ArgumentOutOfRangeException))]
+
+        public void CopyTo_CopyToArrayWithInvalidIndex_ShouldThrowArgumentOutOfRangeException()
+        {
+            var queue = new MyQueue<int>();
+            queue.Enqueue(3);
+            queue.Enqueue(-2);
+            int[] array = new int[2];
+
+            queue.CopyTo(array, -1);
+        }
+
+        [TestMethod]
+        [ExpectedException(typeof(ArgumentException))]
+
+        public void CopyTo_CopyToSmallArray_ShouldThrowArgumentException()
+        {
+            var queue = new MyQueue<int>();
+            queue.Enqueue(3);
+            queue.Enqueue(-2);
+            int[] array = new int[1];
+
+            queue.CopyTo(array, 0);
+        }
+
+        [TestMethod]
+        [ExpectedException(typeof(ArgumentException))]
+
+        public void CopyTo_QueueSize2CopyToArraySize3Index2_ShouldThrowArgumentException()
+        {
+            var queue = new MyQueue<int>();
+            queue.Enqueue(3);
+            queue.Enqueue(-2);
+            int[] array = new int[3];
+
+            queue.CopyTo(array, 2);
+        }
+
+        [TestMethod]
+
+        public void CopyTo_QueueCopyToArrayWithTheSameSize_ArrayEqualQueue()
+        {
+            var queue = new MyQueue<int>();
+            queue.Enqueue(3);
+            queue.Enqueue(-2);
+            int[] array = new int[2];
+
+            queue.CopyTo(array, 0);
+
+            NUnit.Framework.CollectionAssert.AreEqual(array, queue);
+        }
+
+        [TestMethod]
+
+        public void CopyTo_QueueCopyToLargerArray_ArrayIsSupersetOfQueue()
+        {
+            var queue = new MyQueue<int>();
+            queue.Enqueue(3);
+            queue.Enqueue(-2);
+            int[] array = new int[3];
+            array[2] = 100;
+
+            queue.CopyTo(array, 0);
+
+            NUnit.Framework.CollectionAssert.IsSupersetOf(array, queue);
+        }
+
+        [TestMethod]
+
+        public void CopyTo_CopyToNullArrayClass_ShouldThrowArgumentNullException()
+        {
+            var queue = new MyQueue<int>();
+            queue.Enqueue(3);
+            queue.Enqueue(-2);
+            Array array = null;
+
+            try
+            {
+                queue.CopyTo(array, 0);
+            }
+
+            catch (ArgumentNullException)
+            {
+                return;
+            }
+
+            NUnit.Framework.Assert.Fail();
+        }
+
+        [TestMethod]
+        [ExpectedException(typeof(ArgumentOutOfRangeException))]
+
+        public void CopyTo_CopyToArrayClassWithInvalidIndex_ShouldThrowArgumentOutOfRangeException()
+        {
+            var queue = new MyQueue<int>();
+            queue.Enqueue(3);
+            queue.Enqueue(-2);
+            Array array = new int[2];
+
+            queue.CopyTo(array, -1);
+        }
+
+        [TestMethod]
+        [ExpectedException(typeof(ArgumentException))]
+
+        public void CopyTo_CopyToSmallArrayClass_ShouldThrowArgumentException()
+        {
+            var queue = new MyQueue<int>();
+            queue.Enqueue(3);
+            queue.Enqueue(-2);
+            Array array = new int[1];
+
+            queue.CopyTo(array, 0);
+        }
+
+        [TestMethod]
+        [ExpectedException(typeof(ArgumentException))]
+
+        public void CopyTo_QueueSize2CopyToArrayClassSize3Index2_ShouldThrowArgumentException()
+        {
+            var queue = new MyQueue<int>();
+            queue.Enqueue(3);
+            queue.Enqueue(-2);
+            Array array = new int[3];
+
+            queue.CopyTo(array, 2);
+        }
+
+        [TestMethod]
+
+        public void CopyTo_QueueCopyToArrayClassWithTheSameSize_ArrayEqualQueue()
+        {
+            var queue = new MyQueue<int>();
+            queue.Enqueue(3);
+            queue.Enqueue(-2);
+            Array array = new int[2];
+
+            queue.CopyTo(array, 0);
+
+            NUnit.Framework.CollectionAssert.AreEqual(array, queue);
+        }
+
+        [TestMethod]
+
+        public void CopyTo_QueueCopyToLargerArrayClass_ArrayIsSupersetOfQueue()
+        {
+            var queue = new MyQueue<int>();
+            queue.Enqueue(3);
+            queue.Enqueue(-2);
+            Array array = new int[3];
+            array.SetValue(100, 2);
+
+            queue.CopyTo(array, 0);
+
+            NUnit.Framework.CollectionAssert.IsSupersetOf(array, queue);
+        }
+    }
+}
