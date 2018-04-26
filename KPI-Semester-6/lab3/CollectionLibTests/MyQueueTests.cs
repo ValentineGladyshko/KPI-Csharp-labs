c0a4613bb62d5cc53db50e96b00e878d4de70a06
ValentineGladyshko <valentine9022@gmail.com>
2018-04-26 17:16:11 +0300

    Changed tests

--- KPI-Semester-6/lab3/CollectionLibTests/MyQueueTests.cs
+++ KPI-Semester-6/lab3/CollectionLibTests/MyQueueTests.cs
@@ -1,22 +1,22 @@
-﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+﻿using NUnit.Framework;
 using System;
 
 namespace Collection.Lib.Tests
 {
-    [TestClass]
+    [TestFixture]
     public class MyQueueTests
     {
-        [TestMethod]
-        [ExpectedException(typeof(InvalidOperationException))]
+        [Test]
 
         public void Dequeue_DequeueFromEmptyQueue_ShouldThrowInvalidOperationException()
         {
+
             var queue = new MyQueue<int>();
 
-            queue.Dequeue();
+            Assert.Catch(typeof(InvalidOperationException), () => queue.Dequeue()); 
         }
 
-        [TestMethod]
+        [Test]
 
         public void Dequeue_Dequeue3FromQueue_3Returned()
         {
@@ -26,10 +26,10 @@ namespace Collection.Lib.Tests
 
             var num = queue.Dequeue();
 
-            NUnit.Framework.Assert.AreEqual(3, num);
+            Assert.AreEqual(3, num);
         }
 
-        [TestMethod]
+        [Test]
 
         public void Dequeue_Dequeue3FromQueue_QueueIsEmpty()
         {
@@ -38,10 +38,10 @@ namespace Collection.Lib.Tests
 
             queue.Dequeue();
 
-            NUnit.Framework.CollectionAssert.IsEmpty(queue);
+            CollectionAssert.IsEmpty(queue);
         }
 
-        [TestMethod]
+        [Test]
 
         public void Enqueue_Enqueue3ToQueue_3InTheHeadOfQueue()
         {
@@ -50,21 +50,20 @@ namespace Collection.Lib.Tests
             queue.Enqueue(3);
             queue.Enqueue(4);
 
-            NUnit.Framework.Assert.AreEqual(queue.Peek(), 3);
-            //NUnit.Framework.Assert.Contains(3, queue);
+            Assert.AreEqual(queue.Peek(), 3);
+            //Assert.Contains(3, queue);
         }
 
-        [TestMethod]
-        [ExpectedException(typeof(InvalidOperationException))]
+        [Test]
 
         public void Peek_PeekFromEmptyQueue_ShouldThrowInvalidOperationException()
         {
             var queue = new MyQueue<int>();
 
-            queue.Peek();
+            Assert.Catch(typeof(InvalidOperationException), () => queue.Peek());
         }
 
-        [TestMethod]
+        [Test]
 
         public void Peek_PeekFromQueue_3Returned()
         {
@@ -74,10 +73,10 @@ namespace Collection.Lib.Tests
 
             var num = queue.Peek();
 
-            NUnit.Framework.Assert.AreEqual(3, num);
+            Assert.AreEqual(3, num);
         }
 
-        [TestMethod]
+        [Test]
 
         public void ToArray_IntQueueToArray_IntArrayReturned()
         {
@@ -87,10 +86,10 @@ namespace Collection.Lib.Tests
 
             var array = queue.ToArray();
 
-            NUnit.Framework.Assert.IsInstanceOf(typeof(int[]), array);
+            Assert.IsInstanceOf(typeof(int[]), array);
         }
 
-        [TestMethod]
+        [Test]
 
         public void ToArray_QueueWithValuesToArray_ArrayWithValuesReturned()
         {
@@ -100,10 +99,10 @@ namespace Collection.Lib.Tests
 
             var array = queue.ToArray();
 
-            NUnit.Framework.CollectionAssert.AreEqual(array, queue);
+            CollectionAssert.AreEqual(array, queue);
         }
 
-        [TestMethod]
+        [Test]
 
         public void Contains_QueueContains6_TrueReturned()
         {
@@ -113,10 +112,10 @@ namespace Collection.Lib.Tests
 
             var flag = queue.Contains(6);
 
-            NUnit.Framework.Assert.IsTrue(flag);
+            Assert.IsTrue(flag);
         }
 
-        [TestMethod]
+        [Test]
 
         public void Contains_QueueContains6_FalseReturned()
         {
@@ -126,10 +125,10 @@ namespace Collection.Lib.Tests
 
             var flag = queue.Contains(6);
 
-            NUnit.Framework.Assert.IsFalse(flag);
+            Assert.IsFalse(flag);
         }
 
-        [TestMethod]
+        [Test]
 
         public void Clear_ClearQueue_QueueIsEmpty()
         {
@@ -139,10 +138,10 @@ namespace Collection.Lib.Tests
 
             queue.Clear();
 
-            NUnit.Framework.CollectionAssert.IsEmpty(queue);
+            CollectionAssert.IsEmpty(queue);
         }
 
-        [TestMethod]
+        [Test]
 
         public void CopyTo_CopyToNullArray_ShouldThrowArgumentNullException()
         {
@@ -151,21 +150,10 @@ namespace Collection.Lib.Tests
             queue.Enqueue(-2);
             int[] array = null;
 
-            try
-            {
-                queue.CopyTo(array, 0);
-            }
-
-            catch(ArgumentNullException)
-            {
-                return;
-            }
-
-            NUnit.Framework.Assert.Fail();
+            Assert.Catch(typeof(ArgumentNullException), () => queue.CopyTo(array, 0));
         }
 
-        [TestMethod]
-        [ExpectedException(typeof(ArgumentOutOfRangeException))]
+        [Test]
 
         public void CopyTo_CopyToArrayWithInvalidIndex_ShouldThrowArgumentOutOfRangeException()
         {
@@ -174,11 +162,10 @@ namespace Collection.Lib.Tests
             queue.Enqueue(-2);
             int[] array = new int[2];
 
-            queue.CopyTo(array, -1);
+            Assert.Catch(typeof(ArgumentOutOfRangeException), () => queue.CopyTo(array, -1));
         }
 
-        [TestMethod]
-        [ExpectedException(typeof(ArgumentException))]
+        [Test]
 
         public void CopyTo_CopyToSmallArray_ShouldThrowArgumentException()
         {
@@ -187,11 +174,19 @@ namespace Collection.Lib.Tests
             queue.Enqueue(-2);
             int[] array = new int[1];
 
-            queue.CopyTo(array, 0);
+            try
+            {
+                queue.CopyTo(array, 0);
+            }
+            catch (ArgumentException)
+            {
+                return;
+            }
+
+            Assert.Fail();
         }
 
-        [TestMethod]
-        [ExpectedException(typeof(ArgumentException))]
+        [Test]
 
         public void CopyTo_QueueSize2CopyToArraySize3Index2_ShouldThrowArgumentException()
         {
@@ -200,10 +195,19 @@ namespace Collection.Lib.Tests
             queue.Enqueue(-2);
             int[] array = new int[3];
 
-            queue.CopyTo(array, 2);
+            try
+            {
+                queue.CopyTo(array, 2);
+            }
+            catch (ArgumentException)
+            {
+                return;
+            }
+
+            Assert.Fail();
         }
 
-        [TestMethod]
+        [Test]
 
         public void CopyTo_QueueCopyToArrayWithTheSameSize_ArrayEqualQueue()
         {
@@ -214,10 +218,10 @@ namespace Collection.Lib.Tests
 
             queue.CopyTo(array, 0);
 
-            NUnit.Framework.CollectionAssert.AreEqual(array, queue);
+            CollectionAssert.AreEqual(array, queue);
         }
 
-        [TestMethod]
+        [Test]
 
         public void CopyTo_QueueCopyToLargerArray_ArrayIsSupersetOfQueue()
         {
@@ -229,10 +233,10 @@ namespace Collection.Lib.Tests
 
             queue.CopyTo(array, 0);
 
-            NUnit.Framework.CollectionAssert.IsSupersetOf(array, queue);
+            CollectionAssert.IsSupersetOf(array, queue);
         }
 
-        [TestMethod]
+        [Test]
 
         public void CopyTo_CopyToNullArrayClass_ShouldThrowArgumentNullException()
         {
@@ -245,17 +249,15 @@ namespace Collection.Lib.Tests
             {
                 queue.CopyTo(array, 0);
             }
-
             catch (ArgumentNullException)
             {
                 return;
             }
 
-            NUnit.Framework.Assert.Fail();
+            Assert.Fail();
         }
 
-        [TestMethod]
-        [ExpectedException(typeof(ArgumentOutOfRangeException))]
+        [Test]
 
         public void CopyTo_CopyToArrayClassWithInvalidIndex_ShouldThrowArgumentOutOfRangeException()
         {
@@ -264,11 +266,10 @@ namespace Collection.Lib.Tests
             queue.Enqueue(-2);
             Array array = new int[2];
 
-            queue.CopyTo(array, -1);
+            Assert.Catch(typeof(ArgumentOutOfRangeException), ()=> queue.CopyTo(array, -1));
         }
 
-        [TestMethod]
-        [ExpectedException(typeof(ArgumentException))]
+        [Test]
 
         public void CopyTo_CopyToSmallArrayClass_ShouldThrowArgumentException()
         {
@@ -277,11 +278,19 @@ namespace Collection.Lib.Tests
             queue.Enqueue(-2);
             Array array = new int[1];
 
-            queue.CopyTo(array, 0);
+            try
+            {
+                queue.CopyTo(array, 0);
+            }
+            catch (ArgumentException)
+            {
+                return;
+            }
+
+            Assert.Fail();
         }
 
-        [TestMethod]
-        [ExpectedException(typeof(ArgumentException))]
+        [Test]
 
         public void CopyTo_QueueSize2CopyToArrayClassSize3Index2_ShouldThrowArgumentException()
         {
@@ -290,10 +299,19 @@ namespace Collection.Lib.Tests
             queue.Enqueue(-2);
             Array array = new int[3];
 
-            queue.CopyTo(array, 2);
+            try
+            {
+                queue.CopyTo(array, 2);
+            }
+            catch (ArgumentException)
+            {
+                return;
+            }
+
+            Assert.Fail();
         }
 
-        [TestMethod]
+        [Test]
 
         public void CopyTo_QueueCopyToArrayClassWithTheSameSize_ArrayEqualQueue()
         {
@@ -304,10 +322,10 @@ namespace Collection.Lib.Tests
 
             queue.CopyTo(array, 0);
 
-            NUnit.Framework.CollectionAssert.AreEqual(array, queue);
+            CollectionAssert.AreEqual(array, queue);
         }
 
-        [TestMethod]
+        [Test]
 
         public void CopyTo_QueueCopyToLargerArrayClass_ArrayIsSupersetOfQueue()
         {
@@ -319,7 +337,7 @@ namespace Collection.Lib.Tests
 
             queue.CopyTo(array, 0);
 
-            NUnit.Framework.CollectionAssert.IsSupersetOf(array, queue);
+            CollectionAssert.IsSupersetOf(array, queue);
         }
     }
 }
